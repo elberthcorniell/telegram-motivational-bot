540a9f74fab4cda8721f1d76f03f10bf2cc8fd63
Elbert Corniell <elberthcorniell@gmail.com>
2020-11-27 11:32:21 -0400

    /start added

--- lib/bot.rb
+++ lib/bot.rb
@@ -6,6 +6,19 @@ class TelegramBot
         file = File.read('./config.json')
         data = JSON.parse(file)
         token = data["telegram_bot_api"]
-        puts token
+
+        Telegram::Bot::Client.run(token) do |bot|
+            bot.listen do |message|
+                case message.text
+                when '/start'
+                    bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, 
+                    I know sometimes is hard to keed your motivation. I'm here to help you, 
+                    i'll send you a motivational quote everyday. if you want a quote just ask for it typing \"/quote\". 
+                    Else if you feel your motivation will never go down and don'\t need me anymore you can type \"/stop\" 
+                    and I will understand you.")
+                
+            end
+          end
+
     end
 end
