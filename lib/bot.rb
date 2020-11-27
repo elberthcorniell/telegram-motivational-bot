a87d23888f1551197a7cdc057d4156fc7c271404
Elbert Corniell <elberthcorniell@gmail.com>
2020-11-27 11:43:12 -0400

    Creates quoter

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,13 +1,20 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Style/ClassVar
+
 require 'telegram/bot'
+require './quotes'
 require 'json'
 
 class TelegramBot
     def initialize
+        @@chats = []
+        @@quoter = 
         file = File.read('./config.json')
         data = JSON.parse(file)
-        token = data["telegram_bot_api"]
+        @@token = data["telegram_bot_api"]
 
-        Telegram::Bot::Client.run(token) do |bot|
+        Telegram::Bot::Client.run(@@token) do |bot|
             bot.listen do |message|
                 case message.text
                 when '/start'
@@ -21,4 +28,16 @@ class TelegramBot
           end
 
     end
+    def broadcast do
+        quote = @@quoter.get
+        Telegram::Bot::Client.run(@@token) do |bot|
+            @@chats.each do |chat| do
+                bot.api.send_message(chat_id: chat, text: quote)
+            end
+        end
+    end
 end
+
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Style/ClassVars
